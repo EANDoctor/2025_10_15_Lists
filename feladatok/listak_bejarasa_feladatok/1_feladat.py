fdf3d0c3b0ab33ec333e79ed344727e93cf1726f
IvánLevente <ivan.levente@diak.szbi-pg.hu>
2025-10-22 08:45:22 +0200

    Update 1_feladat.py

--- feladatok/listak_bejarasa_feladatok/1_feladat.py
+++ feladatok/listak_bejarasa_feladatok/1_feladat.py
@@ -5,7 +5,7 @@ Készíts egy programot, amely [1; 40] intervallumon kiírja a 3-mal osztható s
 
 oszthato = []
 for i in range(1,41):
-    if i % 3 == 0:
+    if i % 3 == 0 and i % 2 == 0:
         oszthato.append(i)
 
-print(oszthato)
+print(f"hárommal osztható és páros szám: {oszthato}")
